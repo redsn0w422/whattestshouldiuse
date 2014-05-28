747351d77b53a802dd14c1b1f5f04fe4fd9fe3bf
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-05-28 10:24:12 -0500

    added some of the series pictures and some code

--- whattestshouldiuse/whattestshouldiuse.js
+++ whattestshouldiuse/whattestshouldiuse.js
@@ -1,4 +1,32 @@
 if (Meteor.isClient) {
+  Meteor.startup(function () {
+    $("#questionDiv").hide();
+  });
+
+  Template.startBtn.events({
+    'click #startBtn' : function() {
+      $("#questionDiv").show();
+      $("#startBtn_div").hide();
+    }
+  });
+
+  Template.question.question_name = function () {
+    return "questions/nth-term-question.png";
+  }
+
+  Template.question.events({
+    'click #yesBtn' : function () {
+      Template.question.question_name = function () {
+        return "questions/ast.png";
+      }
+    },
+    'click #noBtn' : function () {
+      Template.question.question_name = function () {
+        return "questions/telescoping.png";
+      }
+    }
+  });
+
   Template.hello.greeting = function () {
     return "Welcome to whattestshouldiuse.";
   };
